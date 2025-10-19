3c5214bf32f14d05be4e7f7695b7a90d0be68612
tolia k <tolia.kohan@gmail.com>
2025-10-19 08:45:41 +0300

    implement login and register

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,34 +1,63 @@
 class AuthController < ApplicationController
   ACCESS_EXP_MIN = ENV["JWT_ACCESS_EXPIRATION_MINUTES"].to_i
   REFRESH_EXP_DAYS = ENV["JWT_REFRESH_EXPIRATION_DAYS"].to_i
+
   def login
     email = params[:email]
     password = params[:password]
     client_type = params[:client_type] || "web"
-    user = User.find_by(email: email)
-    if user&.authenticate(password)
-      session = Session.new(user: user, client_type: client_type)
+    device_id = params[:device_id]
+    device_name = params[:device_name]
+    @user = User.find_by(email: email)
+    if @user&.authenticate(password)
+      if client_type == "desktop"
+        if device_id.present?
+          @device = Device.find(device_id)
+          unless @device&.user == @user
+            render json: { error: "Device does not belong to user" }, status: :unauthorized
+            return
+          end
+        else
+          @device = Device.new(user: @user, name: device_name)
+          unless @device.save
+            render json: @device.errors, status: :unprocessable_entity
+            return
+          end
+        end
+      end
+
+      # Changed: attach device to session only for desktop clients
+      session = if client_type == "desktop"
+                  Session.new(user: @user, client_type: client_type, device: @device)
+                else
+                  Session.new(user: @user, client_type: client_type)
+                end
+
       if session.save
-        access_token = JWTUtils.encode_access(
-          {
-            sub: user.id,
-            device_id: session.device_id,
-            jti: session.jti,
-            session_key: session.session_key,
-            exp: ACCESS_EXP_MIN.minutes.from_now.to_i
-          }
-        )
-        refresh_token = JWTUtils.encode_refresh(
-          {
-            sub: user.id,
-            device_id: session.device_id,
-            jti: session.jti,
-            session_key: session.session_key,
-            exp: session.expires_at.to_i
-          }
-        )
+        access_payload = {
+          sub: @user.id,
+          jti: session.jti,
+          session_key: session.session_key,
+          exp: ACCESS_EXP_MIN.minutes.from_now.to_i
+        }
+        refresh_payload = {
+          sub: @user.id,
+          jti: session.jti,
+          session_key: session.session_key,
+          exp: session.expires_at.to_i
+        }
+        if client_type == "desktop"
+          access_payload[:device_id] = session.device_id
+          refresh_payload[:device_id] = session.device_id
+        end
+
+        access_token = JWTUtils.encode_access(access_payload)
+        refresh_token = JWTUtils.encode_refresh(refresh_payload)
+
         set_auth_cookies(access_token, refresh_token)
-        render json: { user_id: user.id, access_token: access_token, refresh_token: refresh_token }, status: :ok
+        res = { user_id: @user.id, access_token: access_token, refresh_token: refresh_token }
+        res["device_id"] = @device.id if @device
+        render json: res, status: :ok
       else
         render json: { error: "Failed to create session" }, status: :internal_server_error
       end
@@ -38,6 +67,25 @@ class AuthController < ApplicationController
   end
 
   def register
+    @user = User.new(user_params)
+    if @user.save
+      session = Session.new(user: user)
+      if session.save
+        access_token = JWTUtils.encode_access(
+          { sub: user.id, jti: session.jti, device_id: session.device_id, exp: ACCESS_EXP_MIN.minutes.from_now.to_i }
+        )
+        refresh_token = JWTUtils.encode_refresh(
+          { sub: user.id, jti: session.jti, device_id: session.device_id, exp: session.expires_at.to_i }
+        )
+        set_auth_cookies(access_token, refresh_token)
+        render json: { user_id: user.id, access_token: access_token, refresh_token: refresh_token, user: user.as_json(except: [:password_digest]) }, status: :created
+      else
+        @user.destroy
+        render json: { error: "Failed to create session" }, status: :internal_server_error
+      end
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
   end
 
   def logout
@@ -47,9 +95,11 @@ class AuthController < ApplicationController
   end
 
   private
+
   def user_params
     params.require(:user).permit(:email, :password, :username)
   end
+
   def set_auth_cookies(access_token, refresh_token)
     cookies.encrypted[:access_token] = {
       value: access_token,
