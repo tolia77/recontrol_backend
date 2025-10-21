568005063da0ce15b866e149ad9fe8d749ff10af
tolia k <tolia.kohan@gmail.com>
2025-10-21 22:55:49 +0300

    configure cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,12 +5,14 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins "example.com"
-#
-#     resource "*",
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    # Allow requests from any origin in development. Change to specific origins in production.
+    origins "127.0.0.1:5175", "localhost:5175"
+
+    resource "*",
+      headers: :any,
+      methods: [ :get, :post, :put, :patch, :delete, :options, :head ],
+      credentials: true
+  end
+end
