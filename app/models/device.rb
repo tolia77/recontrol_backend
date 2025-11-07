185024714ab7c3d3f45b6e7edcc45ed1fc69e2a3
tolia k <tolia.kohan@gmail.com>
2025-11-08 01:09:32 +0200

    fix device sessions destroy

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,6 +1,7 @@
 class Device < ApplicationRecord
   belongs_to :user, required: true
   has_many :device_shares, dependent: :destroy
+  has_many :sessions, dependent: :destroy
   validates :name, presence: true, length: { minimum: 1, maximum: 32 }
   validates :status, presence: true, inclusion: { in: %w(active inactive) }
   attribute :status, default: "inactive"
