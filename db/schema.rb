64c47cadbefaba2aea514ba56d9e5364ee7e38ee
tolia k <tolia.kohan@gmail.com>
2026-03-27 01:03:18 +0200

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_11_04_222055) do
+ActiveRecord::Schema[8.0].define(version: 2026_03_24_232820) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
   enable_extension "pgcrypto"
@@ -35,6 +35,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_11_04_222055) do
     t.datetime "last_active_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "platform_name"
+    t.string "platform_version"
     t.index ["user_id"], name: "index_devices_on_user_id"
   end
 
